32f2d304a330f69d53fca7548e6f3495fc87d08c
christianhasselstrom <crellecrelle@gmail.com>
2016-12-06 10:07:10 +0100

    db changsssee

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 //var url = 'mongodb://localhost:27017/store';
     //mongodb : 'mongodb://clbokea:1234@ds053964.mlab.com:53964/store_2'
+    //mongodb://<dbuser>:<dbpassword>@ds127428.mlab.com:27428/hachhaadv
 module.exports = {
-    mongodb : 'mongodb://clbokea:1234@ds053964.mlab.com:53964/store_2'
+    mongodb : 'mongodb://crellee:1234@ds127428.mlab.com:27428/hachhaadv'
 }
 
