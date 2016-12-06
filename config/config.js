531f4132510d21b2059d8cf214baf15f7b890531
christianhasselstrom <crellecrelle@gmail.com>
2016-12-06 09:58:58 +0100

    db change

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 //var url = 'mongodb://localhost:27017/store';
     //mongodb : 'mongodb://clbokea:1234@ds053964.mlab.com:53964/store_2'
 module.exports = {
-    mongodb : 'mongodb://localhost:27017/store'
+    mongodb : 'mongodb://clbokea:1234@ds053964.mlab.com:53964/store_2'
 }
 
